5a319dd7623c271fe609ad626ea47ec686f65dd0
catobus <catobus@users.noreply.github.com>
2016-06-09 12:53:03 -0700

    Create ImageFromBase64.java

---
+++ src/image/ImageFromBase64.java
@@ -0,0 +1,41 @@
+package image;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class ImageFromBase64 {
+	public static void main(String[] args) {
+		try {
+			String base64 = image2String("https://duke.kenai.com/wave/Wave.jpg");
+			
+			BufferedImage image = string2Image(base64);
+			
+			JOptionPane.showMessageDialog(null, "", "Image from base64", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(image));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static BufferedImage string2Image(String base64) throws IOException {
+		byte[] btDataFile = Base64.decodeBase64(base64);	
+		return ImageIO.read(new ByteArrayInputStream(btDataFile));
+	}
+	
+	public static String image2String(String url) throws MalformedURLException, IOException {
+		BufferedImage source = ImageIO.read(new URL(url));
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(source, "jpg", baos);
+		
+		return Base64.encodeBase64String(baos.toByteArray());
+	}
+}
